d9b63d190c2d821035292385354303ed365eca91
gudong <dong.gu@qq.com>
2016-04-07 16:17:26 +0800

    new player and transfer to game board page

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var path = require('path');
 var router = express.Router();
-
+var Player = require('../lib/player');
 
 /* GET home page. */
 router.get('/',  function (req, res) {
-    res.sendfile(path.join(__dirname , '../public/views' , 'gameboard.html'));
+    res.sendfile(path.join(__dirname , '../public/views' , 'index.html'));
 });
 
 router.post('/startgame',  function (req, res) {
-    console.info(req.params.playerName)
-    res.sendfile(path.join(__dirname , '../public/views' , 'gameboard.html'));
+    var playerName = req.body.playerName;
+    console.info(req.body.playerName)
+    
+ 
+    res.render( path.join(__dirname, '../public/views','gameboard'),{'player':new Player(playerName)});
+    //res.sendfile(path.join(__dirname , '../public/views' , 'gameboard.html'));
 });
 
 
