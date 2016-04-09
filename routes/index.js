cc466782ba3c44b9383652323fca0646dec26131
gudong <dong.gu@qq.com>
2016-04-09 17:02:11 +0800

    add Room.js but there are some exceptions

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,44 @@ var express = require('express');
 var path = require('path');
 var router = express.Router();
 var Player = require('../lib/player');
-
+var Room = require('../lib/Room');
 /* GET home page. */
 router.get('/',  function (req, res) {
     res.sendfile(path.join(__dirname , '../public/views' , 'index.html'));
 });
 
-router.post('/startgame',  function (req, res) {
+router.post('/newGame',  function (req, res) {
     var playerName = req.body.playerName;
-    console.info(req.body.playerName)
+    console.info("new Game: " + playerName);
     
+    var player = new Player(playerName);
+    var result = {
+        'game' : new Room(server,player),
+        'player': player
+    };
  
-    res.render( path.join(__dirname, '../public/views','gameboard'),{'player':new Player(playerName)});
-    //res.sendfile(path.join(__dirname , '../public/views' , 'gameboard.html'));
+    res.render( path.join(__dirname, '../public/views','gameboard'),result);
+});
+
+router.post('/joinGame',  function (req, res) {
+    var gameId = req.body.gameId;
+    var playerName = req.body.playerName;
+    
+    console.info("join Game: " + playerName + " join into room:" + gameId);
+    
+    var room = rooms["_"+gameId];
+    if(room){
+        var player = new Player(playerName);
+        room.join(player);
+        var result = {
+           'game' : room,
+           'player': player
+        };
+       
+       res.render( path.join(__dirname, '../public/views','gameboard'),result);
+    }else{
+       res.sendfile(path.join(__dirname , '../public/views' , 'index.html'));
+    }
 });
 
 
